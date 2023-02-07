09b3e589b303913d3c898ec861dceabbacce480e
ShailersSolutions <shailersolutions@gmail.com>
2023-02-07 18:02:59 +0530

    node tutorial

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app  = express();
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  databasee: "nodejs",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+module.exports = con;
+
+const port =  process.env.PORT||3000;
+const appname = '➤ HRMS';
+app.set('view engine','ejs');
+
+app.get('/', (req, res) =>{
+    var data = { title : 'Login '+appname};
+    res.render('base', data);
+});
+app.get('/register', (req, res) =>{
+    var data = { title : 'Registe '+appname};
+    res.render('register', data);
+})
+app.post('/login', function(req, res, next){    
+  var email = req.body.email;
+  var password = req.body.password;
+
+
+    // con.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function (err, rows, fields) {
+    //     if (err) throw err
+    //     if (rows.length <= 0) {
+    //         req.flash('error', 'Please correct enter email and Password!')
+    //         res.redirect('/')
+    //     }else {
+    //         //req.session.loggedin = true;
+    //         //req.session.name = name;
+    //         res.redirect('/home');
+    //     }
+    // })
+
+    // Form.create(f,function(err,newlyCreatedForm){
+    //     if(err){
+    //         console.log(err);
+    //     }else{
+    //         res.redirect("/result");
+    //     }
+    // });
+})
+
+app.listen(port, ()=>{ console.log("Lostening to the server on http://localhost:3000")});
